c3c5b46314549e01bb957f7369d18363a6eb25a4
Ayberk Güngör <ayberkbilisim@gmail.com>
2021-08-05 15:53:55 +0300

    Added support for async functions

--- customshell.py
+++ customshell.py
@@ -3,31 +3,44 @@ if __name__ == "__main__":
     exit(1)
 
 import shlex
+import asyncio
+
 
 class CustomShell:
-    def __init__(self,begin=">",unknowncommandstr="Unknown command."):
+    def __init__(self,begin=">",cmdnotfound="Unknown command."):
         self._commandlist = {
-            "exit":self.exit
+            "exit":{
+                "async":False,
+                "func": self.exit
+            }
+
         }
         self._begin = begin
         self._exitrequest = False
-        self._uks = unknowncommandstr
+        self._uks = cmdnotfound
         pass
     def command(self,func):
         if func.__name__ in self._commandlist:
             raise TypeError(f"{func.__name__} is already a command")
-        self._commandlist[func.__name__] = func
+        self._commandlist[func.__name__] = {
+            "func": func,
+            "async": asyncio.iscoroutinefunction(func)
+        }
+    
     def exit(self):
         self._exitrequest = True
-    def run(self):
+    async def run(self):
         while not self._exitrequest:
-            print(self._begin,end=" ")
+            print(self._begin,end=" ",flush=True)
             try:
                 strin = shlex.split(input())
                 if strin[0] not in self._commandlist:
-                    print(self._uks)
+                    print(self._uks,flush=True)
                     continue
-                self._commandlist[strin[0]](*strin[1::])
+                if self._commandlist[strin[0]]["async"]:
+                    await self._commandlist[strin[0]]["func"](*strin[1::])
+                else:
+                    self._commandlist[strin[0]]["func"](*strin[1::])
             except KeyboardInterrupt:
                 self.exit()
             except Exception as e:
